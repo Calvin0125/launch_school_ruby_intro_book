d80f8ea66b81a53793e4197b98662e673d8d526c
Calvin Conley <cconley0125@gmail.com>
2020-06-21 14:34:57 -0400

    chapter 2 exerciese

---
+++ variables/name.rb
@@ -0,0 +1,8 @@
+puts "Please enter your first name."
+first_name = gets.chomp
+puts "Plese enter your last name."
+last_name = gets.chomp
+puts "Hello, #{first_name} #{last_name}."
+10.times do
+  puts "#{first_name} #{last_name}"
+end
